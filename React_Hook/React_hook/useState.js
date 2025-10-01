2793b7c73eedbd83f6564c7d0cf73a7ce4f48213
nguyenminh234 <nhozinboy034@gmail.com>
2025-10-01 19:11:01 +0700

    Add files via upload

---
+++ React_Hook/React_hook/useState.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Bạn đã bấm {count} lần</p>
+      <button onClick={() => setCount(count + 1)}>Bấm</button>
+    </div>
+  );
+}
