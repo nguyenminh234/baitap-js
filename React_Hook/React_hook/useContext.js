f1c942e0238f6a3208bfcc03e2407baf85ec8145
nguyenminh234 <nhozinboy034@gmail.com>
2025-10-01 19:11:40 +0700

    Delete React_Hook/React_hook directory

--- React_Hook/React_hook/useContext.js
+++
@@ -1,6 +0,0 @@
-const ThemeContext = React.createContext("light");
-
-function ThemedButton() {
-  const theme = React.useContext(ThemeContext);
-  return <button className={theme}>Nút theo theme</button>;
-}
